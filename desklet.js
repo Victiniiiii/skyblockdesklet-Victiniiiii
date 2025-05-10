4858a8e3c846fb0e15625be93765c0622c77cc6a
Victiniiiii <victinii494@gmail.com>
2025-05-10 23:13:01 +0300

    Version 1.1 changes: - Added support for multiple items - Ability to change update interval

--- desklet.js
+++ desklet.js
@@ -2,54 +2,180 @@ const Desklet = imports.ui.desklet;
 const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
 const St = imports.gi.St;
-const Gio = imports.gi.Gio;
+const Settings = imports.ui.settings;
+const Lang = imports.lang;
+const Gtk = imports.gi.Gtk;
+const ScrollView = imports.gi.St.ScrollView;
 
 function main(metadata, desklet_id) {
-	return new ScriptOutputDesklet(metadata, desklet_id);
+	return new SkyblockPricesDesklet(metadata, desklet_id);
 }
 
-var ScriptOutputDesklet = class ScriptOutputDesklet extends Desklet.Desklet {
-	constructor(metadata, desklet_id) {
-		super(metadata, desklet_id);
+function SkyblockPricesDesklet(metadata, desklet_id) {
+	this._init(metadata, desklet_id);
+}
 
-		this.metadata = metadata;
+SkyblockPricesDesklet.prototype = {
+	__proto__: Desklet.Desklet.prototype,
 
-		this.box = new St.BoxLayout({ vertical: true });
+	_init: function (metadata, desklet_id) {
+		Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
 
-		this.lastUpdatedLabel = new St.Label({ text: "Last Updated: N/A", name: "lastUpdatedLabel" });
-		this.text = new St.Label({ text: "Loading..." });
+		this.items = ["COMPOST", "ENCHANTED_BREAD", "HOT_POTATO_BOOK"];
+		this.updateInterval = 30;
 
-		this.box.add(this.lastUpdatedLabel);
-		this.box.add(this.text);
+		try {
+			this.settings = new Settings.DeskletSettings(this, metadata.uuid, desklet_id);
 
-		this.setContent(this.box);
+			this.settings.bindProperty(Settings.BindingDirection.IN, "items_list", "itemsList", null, null);
 
+			this.settings.bindProperty(Settings.BindingDirection.IN, "update_interval", "updateInterval", null, null);
+
+			this.settings.bindProperty(Settings.BindingDirection.IN, "apply_button", "applyButton", null, null);
+		} catch (e) {
+			global.logError("Failed to initialize settings: " + e);
+		}
+
+		this.setupUI();
+
+		this.parseItemsList();
 		this.update();
-	}
 
-	update() {
+		this.setupUpdateLoop();
+	},
+
+	parseItemsList: function () {
+		if (this.itemsList && this.itemsList.trim() !== "") {
+			this.items = this.itemsList
+				.split("\n")
+				.map(item => item.trim())
+				.filter(item => item !== "");
+		}
+
+		if (this.items.length === 0) {
+			this.items = ["COMPOST"];
+		}
+	},
+
+	setupUpdateLoop: function () {
+		if (this._updateLoop) {
+			Mainloop.source_remove(this._updateLoop);
+		}
+
+		let interval = this.updateInterval * 60;
+		this._updateLoop = Mainloop.timeout_add_seconds(interval, Lang.bind(this, this.update));
+	},
+
+	setupUI: function () {
+		this.mainBox = new St.BoxLayout({ vertical: true, width: 300 });
+
+		this.lastUpdatedLabel = new St.Label({
+			text: "Last Updated: N/A",
+			style_class: "lastUpdatedLabel",
+		});
+		this.mainBox.add(this.lastUpdatedLabel);
+
+		let separator = new St.BoxLayout({
+			style_class: "separator",
+			height: 1,
+			y_expand: false,
+		});
+		this.mainBox.add(separator);
+
+		this.scrollView = new ScrollView({
+			style_class: "scrollview",
+			x_fill: true,
+			y_fill: true,
+			y_align: St.Align.START,
+		});
+
+		this.contentBox = new St.BoxLayout({ vertical: true });
+		this.scrollView.add_actor(this.contentBox);
+		this.mainBox.add(this.scrollView, { expand: true });
+
+		this.setContent(this.mainBox);
+	},
+
+	on_apply_clicked: function () {
+		global.log("Apply settings button clicked");
+		this.parseItemsList();
+		this.setupUpdateLoop();
+		this.update();
+	},
+
+	update: function () {
 		let deskletPath = this.metadata.path;
 		let scriptPath = GLib.build_filenamev([deskletPath, "script.sh"]);
 
-		let [res, out, err, status] = GLib.spawn_command_line_sync(`/bin/bash "${scriptPath}"`);
+		this.contentBox.destroy_all_children();
 
-		let currentTime = new Date();
-		let currentTimeFormatted = currentTime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", hour12: false });
+		for (let i = 0; i < this.items.length; i++) {
+			let itemName = this.items[i].trim();
+			if (itemName === "") continue;
 
-		let lastUpdatedText = "Last Updated: " + currentTimeFormatted;
+			itemName = itemName.replace(/"/g, '\\"');
 
-		if (res && out) {
-			this.text.set_text(out.toString().trim());
-		} else {
-			this.text.set_text("Error running script.");
+			let command = `/bin/bash "${scriptPath}" "${itemName}"`;
+
+			try {
+				let [res, out] = GLib.spawn_command_line_sync(command);
+
+				if (res && out) {
+					let output = out.toString().trim();
+
+					let itemBox = new St.BoxLayout({
+						vertical: true,
+						style_class: "item-box",
+					});
+
+					let lines = output.split("\n");
+					for (let j = 0; j < lines.length; j++) {
+						let line = lines[j].trim();
+						if (line !== "") {
+							let textLabel = new St.Label({ text: line });
+							itemBox.add(textLabel);
+						}
+					}
+
+					if (i < this.items.length - 1) {
+						let itemSeparator = new St.BoxLayout({
+							style_class: "item-separator",
+							height: 1,
+						});
+						itemBox.add(itemSeparator);
+					}
+
+					this.contentBox.add(itemBox);
+				} else {
+					let errorLabel = new St.Label({
+						text: "Error fetching " + this.items[i],
+					});
+					this.contentBox.add(errorLabel);
+				}
+			} catch (e) {
+				global.logError("Error updating item " + itemName + ": " + e);
+				let errorLabel = new St.Label({
+					text: "Error: " + e.message,
+				});
+				this.contentBox.add(errorLabel);
+			}
 		}
 
-		this.lastUpdatedLabel.set_text(lastUpdatedText);
-
-		// Schedule next run in 1800 seconds (30 minutes)
-		Mainloop.timeout_add_seconds(1800, () => {
-			this.update();
-			return false;
+		let currentTime = new Date();
+		let currentTimeFormatted = currentTime.toLocaleTimeString([], {
+			hour: "2-digit",
+			minute: "2-digit",
+			hour12: false,
 		});
-	}
+		this.lastUpdatedLabel.set_text("Last Updated: " + currentTimeFormatted);
+
+		return true;
+	},
+
+	on_desklet_removed: function () {
+		if (this._updateLoop) {
+			Mainloop.source_remove(this._updateLoop);
+			this._updateLoop = null;
+		}
+	},
 };
--- script.sh
+++ script.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 API_URL="https://api.hypixel.net/skyblock/bazaar"
+ITEM="${1:-COMPOST}"  # Use COMPOST if no item is provided
 
-curl -s "$API_URL" | jq -r '
-  .products.COMPOST.sell_summary[0].pricePerUnit as $sellPrice |
-  .products.COMPOST.buy_summary[0].pricePerUnit as $buyPrice |
-  "COMPOST Buy Price: \($buyPrice) coins\nCOMPOST Sell Price: \($sellPrice) coins"
-'
+data=$(curl -s "$API_URL")
+
+sell_price=$(echo "$data" | jq -r ".products.${ITEM}.sell_summary[0].pricePerUnit")
+buy_price=$(echo "$data" | jq -r ".products.${ITEM}.buy_summary[0].pricePerUnit")
+
+if [[ "$sell_price" == "null" || "$buy_price" == "null" ]]; then
+    echo "Invalid item: $ITEM"
+else
+    echo "$ITEM Buy Price: $buy_price coins"
+    echo "$ITEM Sell Price: $sell_price coins"
+fi
